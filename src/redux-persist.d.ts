415acf56aed0523b43045a7f485eccd5f5fdcf60
sensaj <jafarsalum07@gmail.com>
2026-07-26 06:19:00 +0300

    fix: Fixed resux persist not found error

---
+++ src/redux-persist.d.ts
@@ -0,0 +1,14 @@
+declare module "redux-persist/es/storage" {
+  import storage from "redux-persist/lib/storage";
+  export default storage;
+}
+
+declare module "redux-persist/es/persistReducer" {
+  import persistReducer from "redux-persist/lib/persistReducer";
+  export default persistReducer;
+}
+
+declare module "redux-persist/es/persistStore" {
+  import persistStore from "redux-persist/lib/persistStore";
+  export default persistStore;
+}

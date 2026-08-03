42dba479d7a9f11e50ae0c52d9c4083a45a09631
sensaj <jafarsalum07@gmail.com>
2026-08-03 18:10:57 +0300

    feat: Added AppTextAreaField

--- src/shared/components/form/fields/app.text.field.tsx
+++ src/shared/components/form/fields/app.text.field.tsx
@@ -42,3 +42,44 @@ export function AppTextField<T extends FieldValues>({
     />
   );
 }
+
+export function AppTextAreaField<T extends FieldValues>({
+  name,
+  label,
+  placeholder,
+  control,
+  LeadingIcon,
+  Suffix,
+  rows = 1,
+}: BaseFieldProps<T>) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <div className="w-full flex items-start gap-3 rounded-xl bg-slate-300/30 border border-slate-300 px-4 py-3 focus-within:border-[#182f81] focus-within:ring-1 focus-within:ring-[#182f81] transition">
+              {LeadingIcon}
+              <textarea
+                {...field}
+                placeholder={placeholder}
+                rows={rows}
+                onInput={(e) => {
+                  const textarea = e.currentTarget;
+                  textarea.style.height = "auto";
+                  textarea.style.height = `${textarea.scrollHeight}px`;
+                  field.onChange(e);
+                }}
+                className="w-full resize-none overflow-hidden bg-transparent outline-none text-xs text-gray-800 placeholder:text-gray-400"
+              />
+              {Suffix}
+            </div>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}

20ac94654ccfee2b887959ea1b68ec72cd2a5295
sensaj <jafarsalum07@gmail.com>
2026-08-03 18:13:40 +0300

    feat: Added checked property

--- src/shared/components/form/fields/app.checkbox.field.tsx
+++ src/shared/components/form/fields/app.checkbox.field.tsx
@@ -36,14 +36,15 @@ export function AppCheckboxFormField<T extends FieldValues>({
                     <input
                       id={String(name)}
                       {...field}
+                      checked={!!field.value}
                       disabled={disabled}
                       className="peer sr-only cursor-pointer"
                       type="checkbox"
                     />
-                    <div className="size-6 rounded-lg bg-white border-2 border-blue-900 transition-all duration-300 ease-in-out peer-checked:bg-linear-to-br from-blue-900 to-blue-800 peer-checked:border-0 peer-checked:rotate-12 after:content-[''] after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:w-5 after:h-5 after:opacity-0 after:bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48cG9seWxpbmUgcG9pbnRzPSIyMCA2IDkgMTcgNCAxMiI+PC9wb2x5bGluZT48L3N2Zz4=')] after:bg-contain after:bg-no-repeat peer-checked:after:opacity-100 after:transition-opacity after:duration-300 hover:shadow-[0_0_15px_rgba(59,130,246,0.5)]"></div>
+                    <div className="size-5 rounded-lg bg-white border-2 border-blue-900 transition-all duration-300 ease-in-out peer-checked:bg-linear-to-br from-blue-900 to-blue-800 peer-checked:border-0 peer-checked:rotate-12 after:content-[''] after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:w-5 after:h-5 after:opacity-0 after:bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48cG9seWxpbmUgcG9pbnRzPSIyMCA2IDkgMTcgNCAxMiI+PC9wb2x5bGluZT48L3N2Zz4=')] after:bg-contain after:bg-no-repeat peer-checked:after:opacity-100 after:transition-opacity after:duration-300 hover:shadow-[0_0_15px_rgba(59,130,246,0.5)]"></div>
                   </label>
                   <FormLabel>
-                    <span className="text-sm font-medium text-gray-900">
+                    <span className="text-xs font-medium text-gray-900">
                       {label}
                     </span>
                   </FormLabel>
